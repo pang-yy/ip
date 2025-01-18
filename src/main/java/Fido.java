5bc5f45db9957559b7ff1f28e48b4e4b8b6d2931
pyy <45996894+pang-yy@users.noreply.github.com>
2025-01-19 03:31:13 +0800

    Add echo function

--- src/main/java/Fido.java
+++ src/main/java/Fido.java
@@ -1,15 +1,47 @@
+import java.util.Scanner;
+
 public class Fido {
     private final static String CHATBOT_NAME = "Fido";
+    private final static String DIVIDER = "_".repeat(60);
+    private final static int OUTPUT_INDENT_LEVEL = 4;
 
     public static void main(String[] args) {
-        String greetMsg = String.format("Hello! I'm %s\nWhat can I do for you?", CHATBOT_NAME);
-        String byeMsg = "Bye. Hope to see you again soon!";
-        String seperator = "____________________________________________________________";
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println(seperator);
-        System.out.println(greetMsg);
-        System.out.println(seperator);
-        System.out.println(byeMsg);
-        System.out.println(seperator);
+        printMessage(DIVIDER);
+        printMessage(hello());
+        printMessage(DIVIDER);
+        
+        sc.useDelimiter("\n")
+            .tokens()
+            .map(line -> line.trim())
+            .takeWhile(line -> !line.equalsIgnoreCase("bye"))
+            .map(line -> echo(line))
+            .forEach(out -> {
+                printMessage(DIVIDER);
+                printMessage(out);
+                printMessage(DIVIDER);
+            });
+        sc.close();
+
+        printMessage(DIVIDER);
+        printMessage(bye());
+        printMessage(DIVIDER);
+    }
+
+    public static void printMessage(String out) {
+        System.out.println(" ".repeat(OUTPUT_INDENT_LEVEL) + out);
+    }
+
+    public static String hello() {
+        return String.format("Beep...Hello! I am %s. What can I do for you", CHATBOT_NAME);
+    }
+
+    public static String bye() {
+        return "Have a nice day!";
+    }
+
+    public static String echo(String cmd) {
+        return cmd;
     }
 }

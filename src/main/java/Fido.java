7a2368a612ccb1fb5aeda2b7cd462fd585be4614
pyy <45996894+pang-yy@users.noreply.github.com>
2025-01-19 02:41:20 +0800

    Update greet messages

--- src/main/java/Fido.java
+++ src/main/java/Fido.java
@@ -1,10 +1,15 @@
 public class Fido {
+    private final static String CHATBOT_NAME = "Fido";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String greetMsg = String.format("Hello! I'm %s\nWhat can I do for you?", CHATBOT_NAME);
+        String byeMsg = "Bye. Hope to see you again soon!";
+        String seperator = "____________________________________________________________";
+
+        System.out.println(seperator);
+        System.out.println(greetMsg);
+        System.out.println(seperator);
+        System.out.println(byeMsg);
+        System.out.println(seperator);
     }
 }

dfca1593c9a66d99d4db4fdada80c52499f6aef3
pyy <45996894+pang-yy@users.noreply.github.com>
2025-01-20 20:47:00 +0800

    Combine mark, unmark and delete in switch case

--- src/main/java/InputHandler.java
+++ src/main/java/InputHandler.java
@@ -9,49 +9,51 @@ public class InputHandler {
         this.tasks = new ArrayList<>();
     }
 
-    String action(String[] cmd) throws FidoException {
-        switch(cmd[0].toLowerCase()) {
+    String action(String[] inputs) throws FidoException {
+        String command = inputs[0].toLowerCase();
+        switch(command) {
         case "list":
             return this.tasks.stream()
                 .map(t -> (this.tasks.indexOf(t) + 1) + ". " + t.toString())
                 .reduce("", (x, y) -> x + "\n" + y);
         case "mark":
+        case "unmark":
+        case "delete":
             try {
-                int idx = Integer.parseInt(cmd[1]) - 1;
-                Task newTask = this.tasks.get(idx).mark();
-                this.tasks.set(idx, newTask);
-                return "This task has been marked as done.\n" +
-                    "  " + newTask;
+                int idx = Integer.parseInt(inputs[1]) - 1;
+                if (command.equals("mark")) {
+                    Task newTask = this.tasks.get(idx).mark();
+                    this.tasks.set(idx, newTask);
+                    return "This task has been marked as done.\n" +
+                        "  " + newTask;
+                } else if (command.equals("unmark")) {
+                    Task newTask = this.tasks.get(idx).unmark();
+                    this.tasks.set(idx, newTask);
+                    return "This task has been unmarked.\n" +
+                        "  " + newTask;
+                } else { // delete
+                    Task deletedTask = this.tasks.remove(idx);
+                    return "Following task has been removed.\n" +
+                        "  " + deletedTask;
+                }
             } catch (IndexOutOfBoundsException e) {
                 throw new FidoException(FidoException.ErrorType.NOT_VALID_INDEX);
             } catch (NumberFormatException e) {
                 throw new FidoException(FidoException.ErrorType.NOT_VALID_NUMBER);
             }
-        case "unmark":
-            try {
-                int idx = Integer.parseInt(cmd[1]) - 1;
-                Task newTask = this.tasks.get(idx).unmark();
-                this.tasks.set(idx, newTask);
-                return "This task has been unmarked.\n" +
-                    "  " + newTask;
-            } catch (IndexOutOfBoundsException e) {
-                return "Error: Please provide a valid number.";
-            } catch (NumberFormatException e) {
-                return "Error: Please provide number only.";
-            }
         case "todo":
         case "deadline":
         case "event":
-            if (cmd.length < 2) {
+            if (inputs.length < 2) {
                 throw new FidoException(FidoException.ErrorType.EMPTY_DESCRIPTION);
             }
             Task task;
-            if (cmd[0].toLowerCase().equals("todo")) {
-                task = new Todo(String.join(" ", Arrays.copyOfRange(cmd, 1, cmd.length)));
-            } else if (cmd[0].toLowerCase().equals("deadline")) {
-                task = Deadline.of(String.join(" ", Arrays.copyOfRange(cmd, 1, cmd.length)));
-            } else {
-                task = Event.of(String.join(" ", Arrays.copyOfRange(cmd, 1, cmd.length)));
+            if (command.equals("todo")) {
+                task = new Todo(String.join(" ", Arrays.copyOfRange(inputs, 1, inputs.length)));
+            } else if (command.equals("deadline")) {
+                task = Deadline.of(String.join(" ", Arrays.copyOfRange(inputs, 1, inputs.length)));
+            } else { // event
+                task = Event.of(String.join(" ", Arrays.copyOfRange(inputs, 1, inputs.length)));
             }
             this.tasks.add(task);
             if (this.tasks.size() >= 10) {
@@ -59,17 +61,6 @@ public class InputHandler {
                     "added: " + task;
             }
             return "added: " + task;
-        case "delete":
-            try {
-                int idx = Integer.parseInt(cmd[1]) - 1;
-                Task deletedTask = this.tasks.remove(idx);
-                return "Following task has been removed.\n" +
-                    "  " + deletedTask;
-            } catch (IndexOutOfBoundsException e) {
-                throw new FidoException(FidoException.ErrorType.NOT_VALID_INDEX);
-            } catch (NumberFormatException e) {
-                throw new FidoException(FidoException.ErrorType.NOT_VALID_NUMBER);
-            }
         case "help":
             return this.menu();
         default:
